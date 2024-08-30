f65d640faae90517071649ff11476e6350dea367
eyelevelai <33876565+eyelevelai@users.noreply.github.com>
2024-08-30 12:33:25 -0600

    Moving docker containers

--- modules/opensearch/container/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
-docker build --platform linux/amd64 -t public.ecr.aws/c9r4x6y5/eyelevel/opensearch:latest .
-docker push public.ecr.aws/c9r4x6y5/eyelevel/opensearch:latest
--- modules/redis/container/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker login registry.redhat.io
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
-docker build --platform linux/amd64 -t public.ecr.aws/c9r4x6y5/eyelevel/redis:latest .
-docker push public.ecr.aws/c9r4x6y5/eyelevel/redis:latest
