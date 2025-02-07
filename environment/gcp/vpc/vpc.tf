92b9e3f40b495a6314e8e535670ccc1889abbdd6
yantong1775 <yantong1775@gmail.com>
2025-02-06 22:05:25 -0500

    gke: migrate the vpc from aws to gcp

--- environment/gcp/vpc/vpc.tf
+++ environment/gcp/vpc/vpc.tf
@@ -1,31 +1,53 @@
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
 module "eyelevel_vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.8.1"
+  source  = "terraform-google-modules/network/google"
 
-  name = "${local.cluster_name}-vpc"
+  project_id             = var.environment.project_id
+  network_name           = "${local.cluster_name}-vpc"
+  routing_mode           = "GLOBAL"
+  auto_create_subnetworks = false
 
-  cidr = var.vpc.cidr
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  subnets = [
+    {
+      subnet_name  = "gke-subnet"
+      subnet_ip = "10.0.0.0/24"
+      subnet_region = var.environment.region
+    }
+  ]
 
-  private_subnets = var.vpc.private_subnets
-  public_subnets  = var.vpc.public_subnets
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
+  secondary_ranges = {
+    gke-subnet = [
+      {
+        range_name    = "gke-pods-range"
+        ip_cidr_range = "10.1.0.0/20"   # IP range for Pods
+      },
+      {
+        range_name   = "gke-services-range"
+        ip_cidr_range = "10.1.16.0/20"   # IP range for Services
+      }
+    ]
   }
 
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
+  ingress_rules = {
+    name = "allow_ssh_vpc_only"
+    description = "firewall ingress rule to allow ssh from vpc subnets only"
+  
+    allow = [
+      {
+        protocol = "tcp"
+        ports    = ["22"]
+      }
+    ]
+  }
+
+  egress_rules = {
+    name = "allow_all"
+    description = "firewall egress rule to allow all traffic"
+  
+    allow = [
+      {
+        protocol = "all"
+        ports    = ["0.0.0.0/0"]
+      }
+    ]
   }
 }
