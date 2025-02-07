00a16b71ad49625e23c6c31b9b6827d3c7742086
yantong1775 <yantong1775@gmail.com>
2025-02-07 10:04:16 -0500

    gke add config_path for kube_config

--- environment/gcp/common.tf
+++ environment/gcp/common.tf
@@ -2,6 +2,8 @@
 data "google_client_config" "default" {}
 
 provider "kubernetes" {
+  config_path = var.cluster.kube_config_path
+
   host                   = "https://${module.gke.endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
