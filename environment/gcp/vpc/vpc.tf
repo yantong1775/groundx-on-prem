8bcc7b3f997e76582fdd3ee83dfbacc87bf74de6
yantong1775 <yantong1775@gmail.com>
2025-02-09 15:01:02 -0500

    update vpc

--- environment/gcp/vpc/vpc.tf
+++ environment/gcp/vpc/vpc.tf
@@ -1,53 +1,49 @@
-module "eyelevel_vpc" {
-  source  = "terraform-google-modules/network/google"
-
-  project_id             = var.environment.project_id
-  network_name           = "${local.cluster_name}-vpc"
-  routing_mode           = "GLOBAL"
+resource "google_compute_network" "vpc" {
+  name                    = "${local.cluster_name}-vpc"
+  project                 = var.environment.project_id
+  routing_mode = "GLOBAL"
   auto_create_subnetworks = false
+}
 
-  subnets = [
-    {
-      subnet_name  = "gke-subnet"
-      subnet_ip = "10.0.0.0/24"
-      subnet_region = var.environment.region
-    }
-  ]
+resource "google_compute_subnetwork" "gke-subnet" {
+  name          = "gke-subnet"
+  project       = var.environment.project_id
+  region        = var.environment.region
+  network       = google_compute_network.vpc.self_link
+  ip_cidr_range = "10.0.0.0/24"
 
-  secondary_ranges = {
-    gke-subnet = [
-      {
-        range_name    = "gke-pods-range"
-        ip_cidr_range = "10.1.0.0/20"   # IP range for Pods
-      },
-      {
-        range_name   = "gke-services-range"
-        ip_cidr_range = "10.1.16.0/20"   # IP range for Services
-      }
-    ]
+  secondary_ip_range {
+    range_name    = "gke-pods-range"
+    ip_cidr_range = "10.1.0.0/20"  # Range for Pods (4096 addresses)
   }
 
-  ingress_rules = {
-    name = "allow_ssh_vpc_only"
-    description = "firewall ingress rule to allow ssh from vpc subnets only"
-  
-    allow = [
-      {
-        protocol = "tcp"
-        ports    = ["22"]
-      }
-    ]
+  secondary_ip_range {
+    range_name    = "gke-services-range"
+    ip_cidr_range = "10.1.16.0/20" # Range for Services (4096 addresses)
+  }
+}
+
+resource "google_compute_firewall" "allow-ssh-only-ingress" {
+  name    = "allow-ssh-only"
+  network = google_compute_network.vpc.self_link
+  description = "Allow SSH from vpc subnets"
+  direction = "INGRESS"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
+
+resource "google_compute_firewall" "allow-all-egress" {
+  name    = "allow-all-egress"
+  network = google_compute_network.vpc.self_link
+  description = "Allow all traffic"
+  direction = "EGRESS"
+
+  allow {
+    protocol = "all"
   }
 
-  egress_rules = {
-    name = "allow_all"
-    description = "firewall egress rule to allow all traffic"
-  
-    allow = [
-      {
-        protocol = "all"
-        ports    = ["0.0.0.0/0"]
-      }
-    ]
-  }
+  destination_ranges = ["0.0.0.0/0"]
 }
