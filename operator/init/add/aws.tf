67499f31dad0f111e0c1271d4cd5f9ed4b32b121
yantong1775 <yantong1775@gmail.com>
2025-02-11 23:11:22 -0500

    aws storage, not needed

--- operator/init/add/aws.tf
+++ operator/init/add/aws.tf
@@ -1,18 +1,18 @@
-resource "kubernetes_storage_class" "eyelevel_ebs_pv" {
-  count = var.cluster.pv.type == "gp2" && var.cluster.environment == "aws" ? 1 : 0
+# resource "kubernetes_storage_class" "eyelevel_ebs_pv" {
+#   count = var.cluster.pv.type == "gp2" && var.cluster.environment == "aws" ? 1 : 0
 
-  metadata {
-    name = "${var.cluster.pv.name}"
-    annotations = {
-      "storageclass.kubernetes.io/is-default-class" = "true"
-    }
-  }
+#   metadata {
+#     name = "${var.cluster.pv.name}"
+#     annotations = {
+#       "storageclass.kubernetes.io/is-default-class" = "true"
+#     }
+#   }
 
-  storage_provisioner  = "ebs.csi.aws.com"
-  reclaim_policy       = "Delete"
-  volume_binding_mode  = "WaitForFirstConsumer"
+#   storage_provisioner  = "ebs.csi.aws.com"
+#   reclaim_policy       = "Delete"
+#   volume_binding_mode  = "WaitForFirstConsumer"
 
-  parameters = {
-    type = var.cluster.pv.type
-  }
-}
+#   parameters = {
+#     type = var.cluster.pv.type
+#   }
+# }
